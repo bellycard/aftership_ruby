29cc5a09aebc4cc2255eabd27224923b362f8347
Sergii Pylypchuk <psergy@gmail.com>
2013-08-29 13:35:31 +0300

    V3 API fixes

--- lib/aftership/base.rb
+++ lib/aftership/base.rb
@@ -38,20 +38,11 @@ module AfterShip
   module V3
     class Base
       def self.call(http_verb_method, *args)
-        url = "#{AfterShip::URL}/v3/#{args.first.to_s}.#{AfterShip::FORMAT}"
-        req_body = {"aftership-api-key" => AfterShip.api_key_v3}
-        if args.last.is_a?(Hash)
-          args.last.each do |k, v|
-            HTTPI.logger.warn("this field #{k} should be array") if %w(emails smses).include?(k.to_s) && !v.is_a?(Array)
-          end
+        url = "#{AfterShip::URL}/v3/#{args.first.to_s}"
+        headers = {"aftership-api-key" => AfterShip.api_key_v3}
 
-          opt = {}
-          args.last.each {|k, v| v.is_a?(Array) ? opt["#{k}[]"] = v : opt[k] = v}
-          req_body.merge!(opt)
-        end
+        request = HTTPI::Request.new({:url => url, :headers => headers})
 
-        request = HTTPI::Request.new(url)
-        request.body = req_body
         response = HTTPI.send(http_verb_method.to_sym, request)
 
         # different
